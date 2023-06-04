940c1c7648e7dacdfc7eb904ab9601e408309df3
Stephen Buley <stephen.a.buley@gmail.com>
2023-06-04 18:21:10 +0200

    day 1 part 1 complete

--- index.ts
+++ index.ts
@@ -1,10 +1,36 @@
 import * as fs from 'fs'
-async function getInput() {
+function findCalories() {
+  try {
+    const input = fs.readFileSync(`day1Input.txt`, `utf-8`)
+    const lines = input.split('\n')
+    const elves = []
+    let cache = []
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i]
+      if (line === '') {
+        elves.push(cache)
+        cache = []
+      } else {
+        cache.push(line)
+      }
+      if (i == lines.length - 1) {
+        elves.push(cache)
+      }
+    }
 
-    console.log(input)
+    let highest = 0
+    for (const elf of elves) {
+      const total = elf.reduce((acc, val) => {
+        return acc + parseInt(val)
+      }, 0)
+      if (total > highest) {
+        highest = total
+      }
+    }
+    return highest
   } catch (e) {
     console.error(e)
   }
 }
 
-getInput()
+console.log(findCalories())
