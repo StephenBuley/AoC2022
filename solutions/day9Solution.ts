bc176b519366f1133a47c69912b246715a219c89
Stephen Buley <stephen.a.buley@gmail.com>
2023-07-30 23:07:31 +0200

    Day 9 refactor

--- solutions/day9Solution.ts
+++ solutions/day9Solution.ts
@@ -62,45 +62,45 @@ class Knot {
 type Direction = 'U' | 'R' | 'L' | 'D'
 
 export function findTailPositions(numOfKnots: number) {
-  const knots: Knot[] = []
-  for (let i = 0; i < numOfKnots; i++) {
-    let isHead = false
-    let isTail = false
-    if (i === 0) isHead = true
-    if (i === numOfKnots - 1) isTail = true
-
-    knots.push(new Knot(isHead, isTail))
-  }
+  const knots: Knot[] = generateKnotsArray(numOfKnots)
   const tracker = ['00']
-
   for (const instruction of instructions) {
     const [direction, steps] = parseInstructions(instruction)
-
-    for (let i = 0; i < steps; i++) {
-      const head = knots.find((knot) => knot.isHead)!
-      const tail = knots.find((knot) => knot.isTail)!
-      let oldX = head.x
-      let oldY = head.y
-      head.move(direction)
-      let newX = head.x
-      let newY = head.y
-      let currentX
-      let currentY
-      for (let i = 1; i < numOfKnots; i++) {
-        const knot = knots[i]
-        currentX = knot.x
-        currentY = knot.y
-        knot.followHead(oldX, oldY, newX, newY)
-        oldX = currentX
-        oldY = currentY
-        newX = knot.x
-        newY = knot.y
-      }
-      const coords = `${tail.x}${tail.y}`
-      if (!tracker.includes(coords)) tracker.push(coords)
-    }
+    simulateKnotMovements(numOfKnots, knots, direction, steps, tracker)
   }
   return tracker.length
+}
+
+function simulateKnotMovements(
+  numOfKnots: number,
+  knots: Knot[],
+  direction: Direction,
+  steps: number,
+  tracker: string[],
+) {
+  for (let i = 0; i < steps; i++) {
+    const head = knots.find((knot) => knot.isHead)!
+    const tail = knots.find((knot) => knot.isTail)!
+    let oldX = head.x
+    let oldY = head.y
+    head.move(direction)
+    let newX = head.x
+    let newY = head.y
+    let currentX
+    let currentY
+    for (let i = 1; i < numOfKnots; i++) {
+      const knot = knots[i]
+      currentX = knot.x
+      currentY = knot.y
+      knot.followHead(oldX, oldY, newX, newY)
+      oldX = currentX
+      oldY = currentY
+      newX = knot.x
+      newY = knot.y
+    }
+    const coords = `${tail.x}${tail.y}`
+    if (!tracker.includes(coords)) tracker.push(coords)
+  }
 }
 
 function parseInstructions(instruction: string): [Direction, number] {
@@ -108,4 +108,17 @@ function parseInstructions(instruction: string): [Direction, number] {
   const direction = arr[0] as Direction
   const steps = parseInt(arr[1])
   return [direction, steps]
+}
+
+function generateKnotsArray(num: number) {
+  const knots: Knot[] = []
+  for (let i = 0; i < num; i++) {
+    let isHead = false
+    let isTail = false
+    if (i === 0) isHead = true
+    if (i === num - 1) isTail = true
+
+    knots.push(new Knot(isHead, isTail))
+  }
+  return knots
 }
