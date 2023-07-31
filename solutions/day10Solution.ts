a8eeaf6ed03cf2dfefba72ec5deb7592cb8eec22
Stephen Buley <stephen.a.buley@gmail.com>
2023-07-31 08:40:14 +0200

    Day 10 part 1 complete

--- solutions/day10Solution.ts
+++ solutions/day10Solution.ts
@@ -3,7 +3,7 @@ import { getInputFromFile } from '../utils/getInputFromFile'
 const instructions: string[] = []
 
 try {
-  const lines = getInputFromFile('./inputfiles/day10ExampleInput.txt')
+  const lines = getInputFromFile('./inputfiles/day10Input.txt')
 
   if (lines instanceof Error) {
     throw new Error('something bad happened')
@@ -15,5 +15,42 @@ try {
 }
 
 export function findSignalStrength() {
-  console.log(instructions)
+  let cycleNum = 0
+  let X = 1
+  const savedSignalStrengths: number[][] = []
+  for (const instruction of instructions) {
+    const [operation, value] = parseInstructions(instruction)
+    if (operation === 'addx') {
+      cycleNum = cycle(cycleNum, savedSignalStrengths, X)
+      cycleNum = cycle(cycleNum, savedSignalStrengths, X)
+      X += value
+    } else {
+      cycleNum = cycle(cycleNum, savedSignalStrengths, X)
+    }
+  }
+
+  return savedSignalStrengths.reduce((acc, val) => {
+    return (acc += val[0] * val[1])
+  }, 0)
+}
+
+function cycle(cycleNum: number, savedSignalStrengths: number[][], X: number) {
+  cycleNum++
+  saveRelevantSignals(cycleNum, savedSignalStrengths, X)
+  return cycleNum
+}
+
+function saveRelevantSignals(
+  cycleNum: number,
+  savedSignalStrengths: number[][],
+  X: number,
+) {
+  if ((cycleNum - 20) % 40 === 0) {
+    savedSignalStrengths.push([cycleNum, X])
+  }
+}
+
+function parseInstructions(instruction: string): [string, number] {
+  const arr = instruction.split(' ')
+  return [arr[0], parseInt(arr[1]) || 0]
 }
